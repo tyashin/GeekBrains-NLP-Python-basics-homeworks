3ca8434610bf7828a378b937611c7aa864206621
Tim Yashin <t.yashin@tuta.io>
2021-02-09 12:26:21 +0300

    Tak6 6 regexp solution added

--- lesson-3/task_6.py
+++ lesson-3/task_6.py
@@ -4,10 +4,18 @@
 # Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки,
 # но каждое слово должно начинаться с заглавной буквы. Необходимо использовать написанную ранее функцию int_func().
 
+import re
+
+
 def int_func(input_string: str):
 
-    if (list(filter(lambda x: (ord(x) in range(ord('a'), ord('z')+1))
-                    or x.isspace(), input_string))) != list(input_string):
+    # Решение без регулярных выражений
+    # if (list(filter(lambda x: (ord(x) in range(ord('a'), ord('z')+1))
+    #                 or x.isspace(), input_string))) != list(input_string):
+
+    # Решение на regexp'ах
+    p = re.compile('^[a-z\s]+$')
+    if not p.match(input_string):
 
         print('Ошибка. Требовалось ввести строку латинских слов в нижнем регистре разделенных пробелами.')
         return
